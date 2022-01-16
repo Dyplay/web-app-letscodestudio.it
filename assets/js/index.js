b545d1381e8f9bfd043fa641656968704ae07766
Dyplay <71791638+Dyplay@users.noreply.github.com>
2022-01-16 12:48:23 +0100

    web app push

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+function discordlinkmarco() {
+    window.location.replace = 'https://discord.gg/VGPWQWgeVb'
+}
+
+function redictmarcolinktwitter() {
+    window.location.replace = 'https://twitter.com/Gaming5Marco'
+}
---
+++ assets/js/redict.js
@@ -0,0 +1,1 @@
+window.location.href = "/letscodestudio.it web app/view/";
