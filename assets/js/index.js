b3392129401c37a1ab718709655de95078f06792
Dyplay <71791638+Dyplay@users.noreply.github.com>
2022-01-17 22:44:15 +0100

    psuh orgin

--- assets/js/index.js
+++
@@ -1,7 +0,0 @@
-function discordlinkmarco() {
-    window.location.replace = 'https://discord.gg/VGPWQWgeVb'
-}
-
-function redictmarcolinktwitter() {
-    window.location.replace = 'https://twitter.com/Gaming5Marco'
-}
